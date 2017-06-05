f9ea502bc8b1851bd53d154403ea6f9984f5222c
Tracy Lee <tlee@inxpo.com>
2017-06-04 19:08:48 -0500

    child routes config

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-signin',
-    templateUrl: './signup.component.html'
+    templateUrl: './signin.component.html'
 })
 
 export class SigninComponent {
-    onLogout() {
-        
-    }
+
 }
