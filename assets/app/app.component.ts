ee3a4f6a4a6bbb7e91d06de9814eb0938eb0f367
Tracy Lee <tlee@inxpo.com>
2017-06-05 20:46:33 -0500

    modules/lazyloading

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { MessageService } from './messages/message.service';
-import { MessageComponent } from './messages/message.component';
-import { Message } from './messages/message.model';
+
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    providers: [MessageService]
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-    
-}
+
+export class AppComponent {}
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,47 +1,35 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MessageModule } from './messages/message.module';
 
 import { AuthService } from './auth/auth.service';
 import { ErrorService } from './errors/error.service';
 
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from './auth/authentication.component';
-import { SignupComponent } from './auth/signup.compenent';
-import { SigninComponent } from './auth/signin.component';
-import { LogoutComponent } from './auth/logout.compoenent';
 import { routing } from './app.routing';
 import { HeaderComponent } from './header.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageComponent } from './messages/message.component';
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
 import { ErrorComponent } from './errors/error.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [
-        BrowserModule, 
-        FormsModule, 
+        BrowserModule,
         routing, 
-        ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        MessageModule
     ],
-    providers: [AuthService, ErrorService],
-    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+    providers: [
+        AuthService, 
+        ErrorService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
