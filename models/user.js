82dadc1a05a12bfc7c09cf0522b3ddb9d1b9ea42
Tracy Lee <tlee@inxpo.com>
2017-06-08 11:58:20 -0500

    removed server side val

--- models/user.js
+++ models/user.js
@@ -5,16 +5,7 @@ var mongooseUniqueValidator = require('mongoose-unique-validator');
 var schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  password: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function(v) {
-        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(v);
-      },
-      message: 'Password format is invalid. It must contain more than 8 characters, one lowercase letter, one uppercase letter, and one number'
-    }
-  },
+  password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
 });
